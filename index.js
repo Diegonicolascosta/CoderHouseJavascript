02c9c21e63b9fea3901ac44ef4d0db4b20746844
DiegoCosta <dienicosta@gmail.com>
2022-07-11 11:39:25 -0300

    pequeño programa con prompt

---
+++ index.js
@@ -0,0 +1,7 @@
+let name = prompt("Dime tu nombre");                                                                     //Pido Nombre y apellido
+let LastName = prompt("Dime tu aperllido");
+
+let num1 = prompt ("Ingrese primer numero");                                                             //Pido numeros
+let num2 = prompt ("ingrese segundo numero");
+
+alert(name +" "+ LastName +" "+ "tu numero es:" +" "+ parseInt(num1) + parseInt(num2));                     //Muestro resultados
